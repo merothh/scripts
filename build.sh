c8ee5fb1c682b09e0a760f841f6ab39f25972b67
Dyneteve <sound0020@gmail.com>
2019-08-12 19:34:51 +0300

    buildscript: random formatting and whatnot
    * use $() instead of backticks
    * remove unecessary semicolons
    * random spacing

--- build.sh
+++ build.sh
@@ -39,8 +39,8 @@ build() {
     export USE_CCACHE=1
 
     cd $DEVICEPATH_SCR
-    mk_scr=`grep .mk AndroidProducts.mk | cut -d "/" -f "2"`
-    product_scr=`grep "PRODUCT_NAME :=" $mk_scr | cut -d " " -f 3`
+    mk_scr=$(grep .mk AndroidProducts.mk | cut -d "/" -f "2")
+    product_scr=$(grep "PRODUCT_NAME :=" $mk_scr | cut -d " " -f 3)
     cd ../../..
 
     printf "%s\n\n" $($cyan)
@@ -113,16 +113,16 @@ check_dependencies() {
 }
 
 print_help() {
-    echo "Usage: `basename $0` [OPTION]";
-    echo "  -s, --sync-android \ Sync current source" ;
-    echo "  -b, --brand \ Brand name" ;
-    echo "  -d, --device \ Device name" ;
-    echo "  -t, --target \ Make target" ;
-    echo "  -c, --clean \ Clean target" ;
-    echo "  -ca, --cleanall \ Clean entire out" ;
-    echo "  -tg, --telegram \ Enable telegram message" ;
-    echo "  -u, --upload \ Enable drive upload" ;
-    echo "  -r, --release \ Enable drive upload, tg msg and clean" ;
+    echo "Usage: $(basename $0) [OPTION]"
+    echo "  -s, --sync-android \ Sync current source"
+    echo "  -b, --brand \ Brand name"
+    echo "  -d, --device \ Device name"
+    echo "  -t, --target \ Make target"
+    echo "  -c, --clean \ Clean target"
+    echo "  -ca, --cleanall \ Clean entire out"
+    echo "  -tg, --telegram \ Enable telegram message"
+    echo "  -u, --upload \ Enable drive upload"
+    echo "  -r, --release \ Enable drive upload, tg msg and clean"
     exit
 }
 
@@ -166,57 +166,57 @@ strip_args() {
         cur_arg=$1
 
         # find arguments of the form --arg=val and split to --arg val
-        if [ -n "`echo $cur_arg | grep -o =`" ]; then
-            cur_arg=`echo $1 | cut -d'=' -f 1`
-            next_arg=`echo $1 | cut -d'=' -f 2`
+        if [ -n "$(echo $cur_arg | grep -o =)" ]; then
+            cur_arg=$(echo $1 | cut -d'=' -f 1)
+            next_arg=$(echo $1 | cut -d'=' -f 2)
         else
             cur_arg=$1
             next_arg=$2
         fi
 
         case $cur_arg in
-            -s | --sync-android )
-                sync_android_scr=1
-                ;;
-            -b | --brand )
-                brand_scr=$next_arg
-                ;;
-            -d | --device )
-                device_scr=$next_arg
-                ;;
-            -t | --target )
-                build_type_scr=$next_arg
-                ;;
-            -tg | --telegram )
-                telegram_scr=1
-                ;;
-            -u | --upload )
-                upload_scr=1
-                ;;
-            -r | --release )
-                telegram_scr=1
-                upload_scr=1
-                clean_scr=1
-                ;;
-            -c | --clean )
-                clean_scr=1
-                ;;
-            -ca | --clean-all )
-                cleanall_scr=1
-                ;;
-            *)
-                validate_arg $cur_arg;
-                if [ $? -eq 0 ]; then
-                    echo "Unrecognised option $cur_arg passed"
+        -s | --sync-android)
+            sync_android_scr=1
+            ;;
+        -b | --brand)
+            brand_scr=$next_arg
+            ;;
+        -d | --device)
+            device_scr=$next_arg
+            ;;
+        -t | --target)
+            build_type_scr=$next_arg
+            ;;
+        -tg | --telegram)
+            telegram_scr=1
+            ;;
+        -u | --upload)
+            upload_scr=1
+            ;;
+        -r | --release)
+            telegram_scr=1
+            upload_scr=1
+            clean_scr=1
+            ;;
+        -c | --clean)
+            clean_scr=1
+            ;;
+        -ca | --clean-all)
+            cleanall_scr=1
+            ;;
+        *)
+            validate_arg $cur_arg
+            if [ $? -eq 0 ]; then
+                echo "Unrecognised option $cur_arg passed"
+                print_help
+            else
+                validate_arg $prev_arg
+                if [ $? -eq 1 ]; then
+                    echo "Argument $cur_arg passed without flag option"
                     print_help
-                else
-                    validate_arg $prev_arg
-                    if [ $? -eq 1 ]; then
-                        echo "Argument $cur_arg passed without flag option"
-                        print_help
-                    fi
                 fi
-                ;;
+            fi
+            ;;
         esac
         prev_arg=$1
         shift
@@ -234,36 +234,36 @@ sync_source() {
 
 upload() {
     case $build_type_scr in
-        bacon)
-            file=$(ls $OUT_SCR/*201*.zip | tail -n 1)
+    bacon)
+        file=$(ls $OUT_SCR/*201*.zip | tail -n 1)
         ;;
-        bootimage)
-            file=$OUT_SCR/boot.img
+    bootimage)
+        file=$OUT_SCR/boot.img
         ;;
-        recoveryimage)
-            file=$OUT_SCR/recovery.img
+    recoveryimage)
+        file=$OUT_SCR/recovery.img
         ;;
-        dtboimage)
-            file=$OUT_SCR/dtbo.img
+    dtboimage)
+        file=$OUT_SCR/dtbo.img
         ;;
-        systemimage)
-            file=$OUT_SCR/system.img
+    systemimage)
+        file=$OUT_SCR/system.img
         ;;
-        vendorimage)
-            file=$OUT_SCR/vendor.img
+    vendorimage)
+        file=$OUT_SCR/vendor.img
+        ;;
     esac
 
     if [ $upload_scr ]; then
         build_date_scr=$(date +%F_%H-%M)
         if [ $build_type_scr != "bacon" ]; then
             cp $file $HOME/buildscript/$build_type_scr"_"$device_scr"-"$build_date_scr.img
-            file=`ls $HOME/buildscript/*.img | tail -n 1`
+            file=$(ls $HOME/buildscript/*.img | tail -n 1)
         fi
 
-        for tries in {1..3}
-        do
+        for tries in {1..3}; do
             id=$(gdrive upload --parent $G_FOLDER $file | grep "Uploaded" | cut -d " " -f 2)
-            zip_name=`echo $file | grep -o '[^/]*$'`
+            zip_name=$(echo $file | grep -o '[^/]*$')
 
             if [ ! -z $id ]; then
                 if [ $telegram_scr ]; then
@@ -283,9 +283,9 @@ upload() {
 
 validate_arg() {
     valid=$(echo $1 | sed s'/^[\-][a-z0-9A-Z\-]*/valid/'g)
-    [ "x$1" == "x$0" ] && return 0;
-    [ "x$1" == "x" ] && return 0;
-    [ "$valid" == "valid" ] && return 0 || return 1;
+    [ "x$1" == "x$0" ] && return 0
+    [ "x$1" == "x" ] && return 0
+    [ "$valid" == "valid" ] && return 0 || return 1
 }
 
 clear
